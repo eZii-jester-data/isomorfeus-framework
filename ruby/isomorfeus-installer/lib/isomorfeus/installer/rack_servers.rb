15a8a5ac6152780812af50346bfbddfa1593ad04
Jan Biedermann <jan@kursator.de>
2019-05-20 23:11:06 +0200

    Support agoo, falcon, iodine and puma rack servers.

---
+++ ruby/isomorfeus-installer/lib/isomorfeus/installer/rack_servers.rb
@@ -0,0 +1,19 @@
+Isomorfeus::Installer.add_rack_server('agoo', {
+  gems: [ { name: 'agoo', version: "~> 2.8.3" } ],
+  start_command: 'bundle exec rackup -s agoo'
+})
+
+Isomorfeus::Installer.add_rack_server('falcon', {
+  gems: [ { name: 'falcon', version: "~> 0.30.0", require: false } ],
+  start_command: 'bundle exec falcon serve'
+})
+
+Isomorfeus::Installer.add_rack_server('iodine', {
+  gems: [ { name: 'falcon', version: "~> 0.7.31", require: false } ],
+  start_command: 'bundle exec iodine'
+})
+
+Isomorfeus::Installer.add_rack_server('puma', {
+  gems: [ { name: 'puma', version: "~> 3.12.1", require: false } ],
+  start_command: 'bundle exec puma'
+})
