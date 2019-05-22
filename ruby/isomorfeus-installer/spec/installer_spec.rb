ceaf4aeb3397c2263b556b2755f5a6cf18d435e1
Jan Biedermann <jan@kursator.de>
2019-05-22 04:40:30 +0200

    Installer specs

---
+++ ruby/isomorfeus-installer/spec/installer_spec.rb
@@ -0,0 +1,74 @@
+require 'spec_helper'
+
+RSpec.describe 'isomorfeus installer' do
+  context 'creating a app' do
+    before do
+      Dir.chdir('spec')
+      Dir.mkdir('test_apps') unless Dir.exist?('test_apps')
+      Dir.chdir('test_apps')
+      FileUtils.rm_rf('morphing') if Dir.exist?('morphing')
+    end
+
+    after do
+      Dir.chdir('..') if Dir.pwd.end_with?('morphing')
+      FileUtils.rm_rf('morphing') if Dir.exist?('morphing')
+      Dir.chdir('..')
+      Dir.chdir('..')
+    end
+
+    it 'it can' do
+      Isomorfeus::Installer::CLI.start(%w[new morphing])
+      Dir.chdir('morphing')
+      expect(File.exist?(File.join('isomorfeus','styles', 'application.css'))).to be true
+      expect(File.exist?(File.join('isomorfeus', 'imports', 'application.js'))).to be true
+      expect(File.exist?(File.join('isomorfeus', 'imports', 'application_common.js'))).to be true
+      expect(File.exist?(File.join('isomorfeus', 'imports', 'application_debug.js'))).to be true
+      expect(File.exist?(File.join('isomorfeus', 'imports', 'application_ssr.js'))).to be true
+      expect(File.exist?(File.join('isomorfeus', 'isomorfeus_loader.rb'))).to be true
+      expect(File.exist?(File.join('owl_init.rb'))).to be true
+      expect(File.exist?(File.join('app_loader.rb'))).to be true
+      expect(File.exist?(File.join('webpack', 'debug.js'))).to be true
+      expect(File.exist?(File.join('webpack', 'development.js'))).to be true
+      expect(File.exist?(File.join('webpack', 'production.js'))).to be true
+      expect(Dir.exist?(File.join('public', 'assets'))).to be true
+      expect(File.exist?('package.json')).to be true
+      expect(File.exist?('Procfile')).to be true
+      expect(File.exist?('ProcfileDebug')).to be true
+      expect(File.exist?('config.ru')).to be true
+      expect(File.exist?('morphing_app.rb')).to be true
+      expect(File.exist?('Gemfile')).to be true
+    end
+  end
+
+  context 'in a new app' do
+    before :all do
+      Dir.chdir('spec')
+      Dir.mkdir('test_apps') unless Dir.exist?('test_apps')
+      Dir.chdir('test_apps')
+      FileUtils.rm_rf('morphing') if Dir.exist?('morphing')
+      Isomorfeus::Installer::CLI.start(%w[new morphing])
+      Dir.chdir('morphing')
+      `env -i PATH=$PATH yarn install`
+      `env -i PATH=$PATH bundle install`
+    end
+
+    after :all do
+      Dir.chdir('..') if Dir.pwd.end_with?('morphing')
+      FileUtils.rm_rf('morphing') if Dir.exist?('morphing')
+      Dir.chdir('..')
+      Dir.chdir('..')
+    end
+
+    it 'can bundle the assets' do
+      `env -i PATH=$PATH yarn run production_build`
+      manifest = Oj.load(File.read(File.join('public', 'assets', 'manifest.json')), mode: :strict)
+      application_js = manifest['application.js']
+      expect(File.exist?(File.join('public', application_js))).to be true
+    end
+
+    it 'can execute tests' do
+      test_result = `env -i PATH="#{ENV['PATH']}" bundle exec rspec`
+      expect(test_result).to include('1 example, 0 failures')
+    end
+  end
+end
---
+++ ruby/isomorfeus-installer/spec/spec_helper.rb
@@ -0,0 +1,23 @@
+require 'fileutils'
+require 'oj'
+require 'thor'
+require 'erb'
+require 'active_support/core_ext/string'
+require 'thor'
+require 'opal-webpack-loader/installer_cli'
+require_relative '../lib/isomorfeus/installer'
+require_relative '../lib/isomorfeus/installer/rack_servers'
+require_relative '../lib/isomorfeus/installer/other_modules'
+require_relative '../lib/isomorfeus/installer/options_mangler'
+require_relative '../lib/isomorfeus/installer/new_project'
+
+Isomorfeus::Installer.module_directories.each do |mod_dir|
+  mod_path = File.realpath(File.join(Isomorfeus::Installer.base_path, mod_dir))
+  modules = Dir.glob('*.rb', base: mod_path)
+  modules.each do |mod|
+    require_relative File.join(mod_path, mod)
+  end
+end
+
+require_relative '../lib/isomorfeus/installer/cli'
+
