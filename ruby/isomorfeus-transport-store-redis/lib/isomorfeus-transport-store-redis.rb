2381d1dd75497a265ea4d4a87012cb17c68de0fd
Jan Biedermann <jan@kursator.de>
2019-06-17 06:11:43 +0200

    No need for a separate transport store, use iodine.

--- ruby/isomorfeus-transport-store-redis/lib/isomorfeus-transport-store-redis.rb
+++
@@ -1,7 +0,0 @@
-if RUBY_ENGINE == 'opal'
-  # nothing
-else
-  require 'isomorfeus/transport/subscription_store/redis/version'
-  require 'isomorfeus/transport/subscription_store/redis/config'
-  require 'isomorfeus/transport/subscription_store/redis'
-end
--- ruby/isomorfeus-transport-store-redis/lib/isomorfeus/transport/subscription_store/redis/version.rb
+++
@@ -1,9 +0,0 @@
-module Isomorfeus
-  module Transport
-    module SubscriptionStore
-      class Redis
-        VERSION = File.read(File.expand_path("../../../../../../../ISOMORFEUS_VERSION", __dir__)).strip
-      end
-    end
-  end
-end
