9d6d5fe48487eb89af48168535d2da35663a78aa
Jan Biedermann <jan@kursator.de>
2019-06-27 19:05:12 +0200

    remove http support

--- ruby/isomorfeus-transport/lib/isomorfeus-transport-http.rb
+++
@@ -1,7 +0,0 @@
-require 'isomorfeus-transport'
-
-if RUBY_ENGINE == 'opal'
-  require 'isomorfeus/transport/http'
-else
-  Opal.append_path(__dir__.untaint) unless Opal.paths.include?(__dir__.untaint)
-end
