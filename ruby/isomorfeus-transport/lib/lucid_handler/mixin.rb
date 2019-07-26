e786e10af36a0936ca281116d0cd3566adfb8a09
Jan Biedermann <jan@kursator.de>
2019-07-26 04:52:22 +0200

    Handler.on_request

--- ruby/isomorfeus-transport/lib/lucid_handler/mixin.rb
+++ ruby/isomorfeus-transport/lib/lucid_handler/mixin.rb
@@ -2,6 +2,14 @@ module LucidHandler
   module Mixin
     def self.included(base)
       Isomorfeus.add_valid_handler_class(base) unless base == LucidHandler::Base
+
+      base.instance_exec do
+        def on_request(&block)
+          define_method :process_request do |*args|
+            block.call(*args)
+          end
+        end
+      end
     end
   end
 end
