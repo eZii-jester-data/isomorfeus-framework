2c64599fac0c64770b25749123fec5df144ebea0
Jan Biedermann <jan@kursator.de>
2019-08-12 17:18:12 +0200

    Adapt for transport changes

--- ruby/isomorfeus-operation/lib/isomorfeus/operation/handler/operation_handler.rb
+++ ruby/isomorfeus-operation/lib/isomorfeus/operation/handler/operation_handler.rb
@@ -9,15 +9,14 @@ module Isomorfeus
             if Isomorfeus.valid_operation_class_name?(operation_class_name)
               operation_class = Isomorfeus.cached_operation_class(operation_class_name)
               if operation_class
-                request[operation_class_name].each_key do |props_json|
-                  begin
-                    props = Oj.load(props_json, mode: :strict)
-                    props.merge!({pub_sub_client: pub_sub_client, session_id: session_id, current_user: current_user})
-                    operation_promise = operation_class.run(props)
-                    operation_result = nil
-                    operation_promise = operation_promise.then do |result|
-                      operation_result = result
-                    end
+                props_json = request[operation_class_name]
+                begin
+                  props = Oj.load(props_json, mode: :strict)
+                  props.merge!({pub_sub_client: pub_sub_client, session_id: session_id, current_user: current_user})
+                  operation_promise = operation_class.promise_run(props)
+                  if operation_promise.realized?
+                    result = { success: 'ok' , result: operation_promise.value }
+                  else
                     start = Time.now
                     timeout = false
                     while !operation_promise.realized?
@@ -30,15 +29,15 @@ module Isomorfeus
                     if timeout
                       result = { error: 'Timeout' }
                     else
-                      result = { success: 'ok' , result: operation_result}
+                      result = { success: 'ok' , result: operation_promise.value }
                     end
-                  rescue Exception => e
-                    result = if Isomorfeus.production?
-                               { error: { operation_class_name => 'No such thing!' }}
-                             else
-                               { error: { operation_class_name => e.message }}
-                             end
                   end
+                rescue Exception => e
+                  result = if Isomorfeus.production?
+                             { error: { operation_class_name => e.message }}
+                           else
+                             { error: { operation_class_name => e.message }}
+                           end
                 end
               else
                 result = { error: { operation_class_name => 'No such thing!' }}
