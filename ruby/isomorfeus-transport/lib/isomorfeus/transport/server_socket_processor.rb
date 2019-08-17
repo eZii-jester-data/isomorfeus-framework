a85849f71782b492c00f7f97e78dae7974d639de
Jan Biedermann <jan@kursator.de>
2019-08-18 00:58:58 +0200

    Fix client missing bug.

--- ruby/isomorfeus-transport/lib/isomorfeus/transport/server_socket_processor.rb
+++ ruby/isomorfeus-transport/lib/isomorfeus/transport/server_socket_processor.rb
@@ -5,7 +5,7 @@ module Isomorfeus
 
       def on_message(client, data)
         request_hash = Oj.load(data, mode: :strict)
-        result = process_request(client, user, request_hash)
+        result = process_request(client, user(client), request_hash)
         client.write Oj.dump(result, mode: :strict)
       end
 
@@ -21,7 +21,7 @@ module Isomorfeus
         # nothing for now
       end
 
-      def user
+      def user(client)
         client.instance_variable_get(:@isomorfeus_user) || Anonymous.new
       end
     end
