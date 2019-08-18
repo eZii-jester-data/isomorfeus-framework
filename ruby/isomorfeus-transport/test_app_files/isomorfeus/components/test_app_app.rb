762ac15191016c0cd916a43c70a028f7df5c0c6e
Jan Biedermann <jan@kursator.de>
2019-08-18 04:54:16 +0200

    Update components of test apps.

--- ruby/isomorfeus-transport/test_app_files/isomorfeus/components/test_app_app.rb
+++ ruby/isomorfeus-transport/test_app_files/isomorfeus/components/test_app_app.rb
@@ -3,6 +3,7 @@ class TestAppApp < LucidApp::Base
     Router(location: props.location) do
       Switch do
         Route(path: '/', exact: true, component: HelloComponent.JS[:react_component])
+        Route(path: '/snc', exact: true, component: HelloComponent.JS[:react_component])
         Route(path: '/ssr', exact: true, component: HelloComponent.JS[:react_component])
         Route(path: '/welcome', exact: true, component: WelcomeComponent.JS[:react_component])
       end
