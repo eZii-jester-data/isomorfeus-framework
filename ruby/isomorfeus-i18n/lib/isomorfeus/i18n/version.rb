cc2a8d5baddab95ba51b48440de1c74bbb2ca88e
Jan Biedermann <jan@kursator.de>
2019-08-19 02:23:09 +0200

    Version 1.0.0.epsilon3

--- ruby/isomorfeus-i18n/lib/isomorfeus/i18n/version.rb
+++ ruby/isomorfeus-i18n/lib/isomorfeus/i18n/version.rb
@@ -1,5 +1,5 @@
 module Isomorfeus
   module I18n
-    VERSION = '1.0.0.epsilon2'
+    VERSION = '1.0.0.epsilon3'
   end
 end
--- ruby/isomorfeus-operation/lib/isomorfeus/operation/version.rb
+++ ruby/isomorfeus-operation/lib/isomorfeus/operation/version.rb
@@ -1,5 +1,5 @@
 module Isomorfeus
   module Operation
-    VERSION = '1.0.0.epsilon2'
+    VERSION = '1.0.0.epsilon3'
   end
 end
