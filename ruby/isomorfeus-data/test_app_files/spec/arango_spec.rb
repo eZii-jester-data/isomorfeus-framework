80a39dd6936a11419f84b7fed9f5c283542099b7
Jan Biedermann <jan@kursator.de>
2019-09-29 03:24:39 +0200

    More specs for arango connection.

--- ruby/isomorfeus-data/test_app_files/spec/arango_spec.rb
+++ ruby/isomorfeus-data/test_app_files/spec/arango_spec.rb
@@ -1,16 +1,35 @@
 require 'spec_helper'
 
 RSpec.describe 'Arango' do
-  it 'can connect to arango' do
+  it 'is configured' do
+    result = on_server do
+      Isomorfeus.arango_configured?
+    end
+    expect(result).to be true
+  end
+
+  it 'configuration is correct' do
+    result = on_server do
+      Isomorfeus.arango_test
+    end
+    expect(result).to eq({:host=>"localhost", :password=>"root", :port=>"8529", :username=>"root", database: 'TestAppAppTest'})
+  end
+
+  it 'is connected' do
     result = on_server do
       Arango.current_server
     end
-    STDERR.puts result
-    expect(result).to be_truthy
+    expect(result).to be_a Arango::Server
     result = on_server do
       Arango.current_database
     end
-    STDERR.puts result
-    expect(result).to be_truthy
+    expect(result).to be_a Arango::Database
+  end
+
+  it 'can connect' do
+    result = on_server do
+      Isomorfeus.connect_to_arango
+    end
+    expect(result).to be_a Arango::Database
   end
 end
