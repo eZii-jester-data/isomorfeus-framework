85d1bfb0ace50bfee79af5f603baf98115f69f43
Jan Biedermann <jan@kursator.de>
2019-10-11 17:33:52 +0200

    More code preparation.

--- ruby/isomorfeus-data/lib/isomorfeus-data-generic.rb
+++ ruby/isomorfeus-data/lib/isomorfeus-data-generic.rb
@@ -1,6 +1,6 @@
 if RUBY_ENGINE == 'opal'
-  require 'lucid_generic_node/mixin'
-  require 'lucid_generic_node/base'
+  require 'lucid_generic_document/mixin'
+  require 'lucid_generic_document/base'
   require 'lucid_generic_edge/mixin'
   require 'lucid_generic_edge/base'
   require 'lucid_generic_collection/mixin'
