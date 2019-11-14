651a4fe37f9ff59309c1d69f5f0878ed35150376
Jan Biedermann <jan@kursator.de>
2019-11-14 09:58:56 +0100

    Update iodine.

--- ruby/isomorfeus/lib/isomorfeus/installer/rack_servers.rb
+++ ruby/isomorfeus/lib/isomorfeus/installer/rack_servers.rb
@@ -4,7 +4,7 @@
 # })
 
 Isomorfeus::Installer.add_rack_server('iodine', {
-  gems: [ { name: 'iodine', version: "~> 0.7.35", require: true } ],
+  gems: [ { name: 'iodine', version: "~> 0.7.37", require: true } ],
   start_command: 'bundle exec iodine',
   init_template: 'iodine_config.rb.erb',
   rack_server_init: "require_relative 'iodine_config'"
