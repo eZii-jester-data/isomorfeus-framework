b6e14e7fdf195e4a76f98af97892065716be9d23
Jan Biedermann <jan@kursator.de>
2019-11-20 15:36:07 +0100

    LucidGenericDocument

--- ruby/isomorfeus-data/lib/lucid_generic_document/mixin.rb
+++ ruby/isomorfeus-data/lib/lucid_generic_document/mixin.rb
@@ -1,141 +1,106 @@
-# api
-# class:
-#   attribute :my_attribute, server_only: false|true, class: ClassName, is_a: ClassName, default: value, validate: block
-#   my_node.class.attributes
-#   my_node.class.attribute_options
-# instance:
-#   my_node.my_attribute
-#   my_node.my_attribute = value
-#   my_node.changed_attributes
-#   my_node.changed?
-#   my_node.loaded?
-#   my_node.valid_attribute?(attr, value)
-#   my_node.validate_attribute!(attr, value)
-#   my_node.to_transport
-
 module LucidGenericDocument
   module Mixin
-    # TODO node -> document
     def self.included(base)
-      base.include(Enumerable)
       base.extend(LucidPropDeclaration::Mixin)
       base.extend(Isomorfeus::Data::GenericClassApi)
       base.include(Isomorfeus::Data::GenericInstanceApi)
 
       base.instance_exec do
         def _handler_type
-          'graph'
+          'document'
         end
-      end
 
-      def ==(other_node)
-        eql?(other_node)
-      end
+        def attribute_conditions
+          @attribute_conditions ||= {}
+        end
 
-      def eql?(other_node)
-        @id == other_node.id && @class_name == other_node.instance_variable_get(:@class_name)
-      end
-
-      def id=(new_id)
-        new_id = new_id.to_s
-        changed_attributes[:id] = new_id
-        @id = new_id
-      end
-
-      def changed_attributes
-        @changed_attributes ||= Isomorfeus::Data::Props.new({})
-      end
-
-      def changed?
-        changed_attributes.any?
-      end
-
-      def to_sid
-        [@class_name, @id]
-      end
-
-      def valid_attribute?(attr, value)
-        begin
-          validate_attribute!(attr, value)
-          true
+        def valid_attribute?(attr_name, attr_value)
+          Isomorfeus::Props::Validator.new(self.name, attr_name, attr_value, attribute_conditions[attr_name]).validate!
         rescue
           false
         end
       end
 
-      def validate_attribute!(attr, value)
-        attr_options = self.class.attribute_options[attr]
-
-        if attr_options.key?(:class)
-          raise "#{attr}: value class is not #{attr_options[:class]}!" unless value.class == attr_options[:class]
-        end
-
-        if attr_options.key?(:is_a)
-          raise "#{attr}: value is not a #{attr_options[:class]}!" unless value.is_a?(attr_options[:is_a])
-        end
-
-        if attr_options.key?(:validate)
-          raise "#{attr}: value failed validation!" unless attr_options[:validate].call(value)
-        end
+      def attribute?(name)
+        self.class.attribute_conditions.key?(name)
       end
 
-      base.instance_exec do
-        def attributes
-          attribute_options.keys
-        end
-
-        def attribute_options
-          @attribute_options ||= { id: {} }
-        end
-
-        def node_from_cid(cid)
-          Isomorfeus.cached_generic_node_class(cid[0]).new({id: cid[1]})
-        end
+      def _validate_attribute(attr_name, attr_val)
+        Isomorfeus::Props::Validator.new(@class_name, attr_name, attr_val, self.class.attribute_conditions[attr_name]).validate!
       end
 
       if RUBY_ENGINE == 'opal'
-        def initialize(attributes_hash = nil)
-          attributes_hash = {} unless attributes_hash
-          self.class.attributes.each do |attr|
-            if attributes_hash.key?(attr)
-              validate_attribute!(attr, attributes_hash[attr])
-              changed_attributes[attr] = attributes_hash[attr]
-            elsif self.class.attribute_options[attr].key?(:default)
-              changed_attributes[attr] = self.class.attribute_options[attr][:default]
-            end
-          end
-          @id = attributes_hash[:id].to_s
-          @id = "new_#{object_id}" if @id.empty?
-          @class_name = self.class.name
-          @class_name = @class_name.split('>::').last if @class_name.start_with?('#<')
-        end
-
-        def to_transport(*args)
-          final_attributes = {}
-          self.class.attributes.each do |attr|
-            next if attr == :id
-            final_attributes[attr] = send(attr)
-          end
-          { 'generic_nodes' => { @class_name => { @id => { attributes: final_attributes }}}}
-        end
-
         base.instance_exec do
           def attribute(name, options = {})
-            attribute_options[name] = options
+            attribute_conditions[name] = options
 
             define_method(name) do
-              if changed_attributes.key?(name)
-                changed_attributes[name]
+              path = @_store_path + [name]
+              result = Redux.fetch_by_path(*path)
+              if result
+                result
+              elsif !@_default_proc
+                @_default
               else
-                Redux.fetch_by_path(:data_state, :generic_nodes, @class_name, @id, :attributes, name)
+                @_default_proc.call(self, name)
               end
             end
 
-            define_method("#{name}=") do |arg|
-              validate_attribute!(name, arg)
-              changed_attributes.set(name, arg)
+            define_method("#{name}=") do |val|
+              _validate_attribute(name, val)
+              _update_attribute(name, val)
+              val
             end
           end
+        end
+
+        def initialize(key:, revision: nil, attributes: nil)
+          @key = key.to_s
+          @class_name = self.class.name
+          @class_name = @class_name.split('>::').last if @class_name.start_with?('#<')
+          @_store_path = [:data_state, @class_name, @key]
+          @_changed_store_path = [:data_state, :changed, @class_name, @key]
+          @_revision_store_path = [:data_state, :revision, @class_name, @key]
+          attributes = {} unless attributes
+          attributes.each { |a,v| _validate_attribute(a, v) }
+          Isomorfeus.store.dispatch(type: 'DATA_LOAD', data: { @class_name => { @key => attributes },
+                                                               changed: { @class_name => { @key => false }},
+                                                               revision: { @class_name => { @key => revision }}})
+        end
+
+        def _update_attribute(attr_name, attr_val)
+          Isomorfeus.store.dispatch(type: 'DATA_LOAD', data: { @class_name => { @key => { attr_name => attr_val }},
+                                                               changed: { @class_name => { @key => true }}})
+        end
+
+        def _update_attributes(hash)
+          Isomorfeus.store.dispatch(type: 'DATA_LOAD', data: { @class_name => { @key => hash},
+                                                               changed: { @class_name => { @key => true }}})
+        end
+
+        def each(&block)
+          raw_attributes = Redux.fetch_by_path(*@_store_path)
+          Hash.new(raw_attributes).each(&block)
+        end
+
+        def [](name)
+          path = @_store_path + [name]
+          result = Redux.fetch_by_path(*path)
+          return result if result
+          nil
+        end
+
+        def []=(name, val)
+          _validate_attribute(name, val)
+          _update_attribute(name, val)
+          val
+        end
+
+        def to_transport
+          raw_hash = Redux.fetch_by_path(*@_store_path)
+          hash = raw_hash ? Hash.new(raw_hash) : {}
+          hash.merge!(_revision: revision)
+          { @class_name => { @key => hash }}
         end
       else # RUBY_ENGINE
         unless base == LucidGenericDocument::Base
@@ -144,53 +109,59 @@ module LucidGenericDocument
           base.prop :current_user, default: Anonymous.new
         end
 
-        def initialize(attributes_hash = nil)
-          attributes_hash = {} unless attributes_hash
-          given_attributes = Isomorfeus::Data::Props.new(attributes_hash)
-          valid_attributes_hash = {}
-          self.class.attributes.each do |attr|
-            if given_attributes.key?(attr)
-              validate_attribute!(attr, given_attributes[attr])
-              valid_attributes_hash[attr] = given_attributes[attr]
-            elsif self.class.attribute_options[attr].key?(:default)
-              valid_attributes_hash[attr] = self.class.attribute_options[attr][:default]
-            end
-          end
-          @attributes = Isomorfeus::Data::Props.new(valid_attributes_hash)
-          @id = @attributes[:id].to_s
-          @id = "new_#{object_id}" if @id.empty?
-          @class_name = self.class.name
-          @class_name = @class_name.split('>::').last if @class_name.start_with?('#<')
-        end
-
-        def to_transport(*args)
-          final_attributes = {}
-          self.class.attributes.each do |attr|
-            next if attr == :id
-            include_attribute = @attributes.key?(attr)
-            include_attribute = !self.class.attribute_options[attr][:server_only] if self.class.attribute_options[attr].key?(:server_only)
-            final_attributes[attr.to_s] = send(attr) if include_attribute
-          end
-          { 'generic_nodes' => { @class_name => { @id => { 'attributes' => final_attributes }}}}
-        end
-
         base.instance_exec do
           def attribute(name, options = {})
-            attribute_options[name] = options
+            attribute_conditions[name] = options
 
             define_method(name) do
-              if changed_attributes.key?(name)
-                changed_attributes[name]
-              else
-                @attributes[name]
-              end
+              @_raw_attributes[name]
             end
 
-            define_method("#{name}=") do |arg|
-              validate_attribute!(name, arg)
-              changed_attributes.set(name, arg)
+            define_method("#{name}=") do |val|
+              _validate_attribute(name, val)
+              @_changed = true
+              @_raw_attributes[name] = val
             end
           end
+        end
+
+        def initialize(key:, revision: nil, attributes: nil)
+          @key = key.to_s
+          @_revision = revision
+          @_changed = false
+          @class_name = self.class.name
+          @class_name = @class_name.split('>::').last if @class_name.start_with?('#<')
+          @_validate_attributes = self.class.attribute_conditions.any?
+          attributes = {} unless attributes
+          if @_validate_attributes
+            attributes.each { |a,v| _validate_attribute(a, v) }
+          end
+          @_raw_attributes = attributes
+        end
+
+        def each(&block)
+          @_raw_attributes.each(&block)
+        end
+
+        def [](name)
+          @_raw_attributes[name]
+        end
+
+        def []=(name, val)
+          _validate_attribute(name, val)
+          @_changed = true
+          @_raw_attributes[name] = val
+        end
+
+        def to_transport
+          hash = {}
+          self.class.attribute_conditions.each do |attr, options|
+            if !options[:server_only] && @_raw_attributes.key?(attr)
+              hash[attr.to_s] = @_raw_attributes[attr]
+            end
+          end
+          hash.merge!("_revision" => revision)
+          { @class_name => { @key => hash }}
         end
       end # RUBY_ENGINE
     end
---
+++ ruby/isomorfeus-data/test_app_files/spec/generic_document_spec.rb
@@ -0,0 +1,336 @@
+require 'spec_helper'
+
+RSpec.describe 'LucidGenericDocument' do
+  context 'on the server' do
+    it 'can instantiate a document by inheritance' do
+      result = on_server do
+        class TestDocumentBase < LucidGenericDocument::Base
+          attribute :test_attribute
+        end
+        document = TestDocumentBase.new(key: 1, attributes: { test_attribute: 'test_value' })
+        document.test_attribute
+      end
+      expect(result).to eq('test_value')
+    end
+
+    it 'can instantiate a document by mixin' do
+      result = on_server do
+        class TestDocumentMixin
+          include LucidGenericDocument::Mixin
+          attribute :test_attribute
+        end
+        document = TestDocumentMixin.new(key: 2, attributes: { test_attribute: 'test_value' })
+        document.test_attribute
+      end
+      expect(result).to eq('test_value')
+    end
+
+    it 'verifies attribute class' do
+      result = on_server do
+        class TestDocumentMixinC < LucidGenericDocument::Base
+          attribute :test_attribute, class: String
+        end
+        document = TestDocumentMixinC.new(key: 3, attributes: { test_attribute: 'test_value' })
+        document.test_attribute.class.name
+      end
+      expect(result).to eq('String')
+      result = on_server do
+        class TestDocumentMixinC < LucidGenericDocument::Base
+          attribute :test_attribute, class: String
+        end
+        begin
+          TestDocumentMixinC.new(key: 4, attributes: { test_attribute: 10 })
+        rescue
+          'exception thrown'
+        end
+      end
+      expect(result).to eq('exception thrown')
+      result = on_server do
+        class TestDocumentMixinC < LucidGenericDocument::Base
+          attribute :test_attribute, class: String
+        end
+        begin
+          document = TestDocumentMixinC.new(key: 5)
+          document.test_attribute = 10
+        rescue
+          'exception thrown'
+        end
+      end
+      expect(result).to eq('exception thrown')
+    end
+
+    it 'verifies if attribute is_a' do
+      result = on_server do
+        class TestDocumentMixinC < LucidGenericDocument::Base
+          attribute :test_attribute, is_a: Enumerable
+        end
+        document = TestDocumentMixinC.new(key: 6, attributes: { test_attribute: ['test_value'] })
+        document.test_attribute.class.name
+      end
+      expect(result).to eq('Array')
+      result = on_server do
+        class TestDocumentMixinC < LucidGenericDocument::Base
+          attribute :test_attribute, is_a: Enumerable
+        end
+        begin
+          TestDocumentMixinC.new(key: 7, attributes: { test_attribute: 10 })
+        rescue
+          'exception thrown'
+        end
+      end
+      expect(result).to eq('exception thrown')
+      result = on_server do
+        class TestDocumentMixinC < LucidGenericDocument::Base
+          attribute :test_attribute, is_a: Enumerable
+        end
+        begin
+          document = TestDocumentMixinC.new(key: 7)
+          document.test_attribute = 10
+        rescue
+          'exception thrown'
+        end
+      end
+      expect(result).to eq('exception thrown')
+    end
+
+    it 'reports a change' do
+      result = on_server do
+        class TestDocumentMixinC < LucidGenericDocument::Base
+          attribute :test_attribute
+        end
+        document = TestDocumentMixinC.new(key: 9, attributes: { test_attribute: 10 })
+        document.changed?
+      end
+      expect(result).to be(false)
+      result = on_server do
+        class TestDocumentMixinC < LucidGenericDocument::Base
+          attribute :test_attribute
+        end
+        document = TestDocumentMixinC.new(key: 10, attributes: { test_attribute: 10 })
+        document.test_attribute = 20
+        document.changed?
+      end
+      expect(result).to be(true)
+    end
+
+    it 'converts to sid' do
+      result = on_server do
+        class TestDocumentMixinC < LucidGenericDocument::Base
+          attribute :test_attribute
+        end
+        document = TestDocumentMixinC.new(key: 11)
+        document.to_sid
+      end
+      expect(result).to eq(['TestDocumentMixinC', '11'])
+    end
+
+    it 'can validate a attribute' do
+      result = on_server do
+        class TestDocumentMixinC < LucidGenericDocument::Base
+          attribute :test_attribute, class: String
+        end
+        TestDocumentMixinC.valid_attribute?(:test_attribute, 10)
+      end
+      expect(result).to eq(false)
+      result = on_server do
+        class TestDocumentMixinC < LucidGenericDocument::Base
+          attribute :test_attribute, class: String
+        end
+        TestDocumentMixinC.valid_attribute?(:test, '10')
+      end
+      expect(result).to eq(false)
+      result = on_server do
+        class TestDocumentMixinC < LucidGenericDocument::Base
+          attribute :test_attribute, class: String
+        end
+        TestDocumentMixinC.valid_attribute?(:test_attribute, '10')
+      end
+      expect(result).to eq(true)
+    end
+
+    it 'converts to transport' do
+      result = on_server do
+        class TestDocumentMixinC < LucidGenericDocument::Base
+          attribute :test_attribute
+        end
+        document = TestDocumentMixinC.new(key: 12, attributes: { test_attribute: 'test'})
+        document.to_transport
+      end
+      expect(result).to eq("TestDocumentMixinC"=>{"12"=>{"_revision"=>nil, "test_attribute" => "test"}})
+    end
+
+    it 'keeps server_only attribute on server' do
+      result = on_server do
+        class TestDocumentMixinC < LucidGenericDocument::Base
+          attribute :test_attribute, server_only: true
+        end
+        document = TestDocumentMixinC.new(key: 13, attributes: { test_attribute: 'test' })
+        document.to_transport
+      end
+      expect(result).to eq("TestDocumentMixinC"=>{"13"=>{"_revision"=>nil}})
+    end
+  end
+
+  context 'on the client' do
+    before :each do
+      @doc = visit('/')
+    end
+
+    it 'can instantiate a document by inheritance' do
+      result = @doc.evaluate_ruby do
+        class TestDocumentBase < LucidGenericDocument::Base
+          attribute :test_attribute
+        end
+        document = TestDocumentBase.new(key: 14, attributes: { test_attribute: 'test_value' })
+        document.test_attribute
+      end
+      expect(result).to eq('test_value')
+    end
+
+    it 'can instantiate a document by mixin' do
+      result = @doc.evaluate_ruby do
+        class TestDocumentMixin
+          include LucidGenericDocument::Mixin
+          attribute :test_attribute
+        end
+        document = TestDocumentMixin.new(key: 15, attributes: { test_attribute: 'test_value' })
+        document.test_attribute
+      end
+      expect(result).to eq('test_value')
+    end
+
+    it 'verifies attribute class' do
+      result = @doc.evaluate_ruby do
+        class TestDocumentMixinC < LucidGenericDocument::Base
+          attribute :test_attribute, class: String
+        end
+        document = TestDocumentMixinC.new(key: 16, attributes: { test_attribute: 'test_value' })
+        document.test_attribute.class.name
+      end
+      expect(result).to eq('String')
+      result = @doc.evaluate_ruby do
+        class TestDocumentMixinC < LucidGenericDocument::Base
+          attribute :test_attribute, class: String
+        end
+        begin
+          TestDocumentMixinC.new(key: 17, attributes: { test_attribute: 10 })
+        rescue
+          'exception thrown'
+        end
+      end
+      expect(result).to eq('exception thrown')
+      result = @doc.evaluate_ruby do
+        class TestDocumentMixinC < LucidGenericDocument::Base
+          attribute :test_attribute, class: String
+        end
+        begin
+          document = TestDocumentMixinC.new(key: 18)
+          document.test_attribute = 10
+        rescue
+          'exception thrown'
+        end
+      end
+      expect(result).to eq('exception thrown')
+    end
+
+    it 'verifies if attribute is_a' do
+      result = @doc.evaluate_ruby do
+        class TestDocumentMixinC < LucidGenericDocument::Base
+          attribute :test_attribute, is_a: Enumerable
+        end
+        document = TestDocumentMixinC.new(key: 19, attributes: { test_attribute: ['test_value'] })
+        document.test_attribute.class.name
+      end
+      expect(result).to eq('Array')
+      result = @doc.evaluate_ruby do
+        class TestDocumentMixinC < LucidGenericDocument::Base
+          attribute :test_attribute, is_a: Enumerable
+        end
+        begin
+          TestDocumentMixinC.new(key: 20, attributes: { test_attribute: 10 })
+        rescue
+          'exception thrown'
+        end
+      end
+      expect(result).to eq('exception thrown')
+      result = @doc.evaluate_ruby do
+        class TestDocumentMixinC < LucidGenericDocument::Base
+          attribute :test_attribute, is_a: Enumerable
+        end
+        begin
+          document = TestDocumentMixinC.new(key: 21)
+          document.test_attribute = 10
+        rescue
+          'exception thrown'
+        end
+      end
+      expect(result).to eq('exception thrown')
+    end
+
+    it 'reports a change' do
+      result = @doc.evaluate_ruby do
+        class TestDocumentMixinC < LucidGenericDocument::Base
+          attribute :test_attribute
+        end
+        document = TestDocumentMixinC.new(key: 23, attributes: { test_attribute: 10 })
+        document.changed?
+      end
+      expect(result).to be(false)
+      result = @doc.evaluate_ruby do
+        class TestDocumentMixinC < LucidGenericDocument::Base
+          attribute :test_attribute
+        end
+        document = TestDocumentMixinC.new(key: 23, attributes: { test_attribute: 10 })
+        document.test_attribute = 20
+        document.changed?
+      end
+      expect(result).to be(true)
+    end
+
+    it 'converts to sid' do
+      result = @doc.evaluate_ruby do
+        class TestDocumentMixinC < LucidGenericDocument::Base
+          attribute :test_attribute
+        end
+        document = TestDocumentMixinC.new(key: 24)
+        document.to_sid
+      end
+      expect(result).to eq(['TestDocumentMixinC', '24'])
+    end
+
+    it 'can validate a attribute' do
+      result = @doc.evaluate_ruby do
+        class TestDocumentMixinC < LucidGenericDocument::Base
+          attribute :test_attribute, class: String
+        end
+        TestDocumentMixinC.valid_attribute?(:test_attribute, 10)
+      end
+      expect(result).to eq(false)
+      result = @doc.evaluate_ruby do
+        class TestDocumentMixinC < LucidGenericDocument::Base
+          attribute :test_attribute, class: String
+        end
+        TestDocumentMixinC.valid_attribute?(:test, '10')
+      end
+      expect(result).to eq(false)
+      result = @doc.evaluate_ruby do
+        class TestDocumentMixinC < LucidGenericDocument::Base
+          attribute :test_attribute, class: String
+        end
+        TestDocumentMixinC.valid_attribute?(:test_attribute, '10')
+      end
+      expect(result).to eq(true)
+    end
+
+    it 'converts to transport' do
+      result = @doc.evaluate_ruby do
+        class TestDocumentMixinC < LucidGenericDocument::Base
+          attribute :test_attribute
+        end
+        document = TestDocumentMixinC.new(key: 28, attributes: { test_attribute: 'test' })
+        document.to_transport.to_n
+      end
+      expect(result).to eq("TestDocumentMixinC" => {"28"=>{"_revision"=>nil, "test_attribute" => "test"}})
+    end
+  end
+end
